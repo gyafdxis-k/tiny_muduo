dff465bcfb39105070fe837ea93df06baebb311e
高栋 <141558247+gyafdxis-k@users.noreply.github.com>
2024-12-02 02:40:39 +0000

    add thread

---
+++ eventloop_thread.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "noncopyable.h"
+#include <functional>
+#include "thread.h"
+#include "eventloop.h"
+#include <condition_variable>
+#include <mutex>
+#include <string>
+class EventLoopThread : noncopyable {
+
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+    EventLoopThread(const ThreadInitCallback& cb = ThreadInitCallback(),
+            const std::string &name = std::string(""));
+    ~EventLoopThread();
+    EventLoop* startLoop();
+private:
+    void threadFunc();
+    EventLoop *loop_;
+    bool existing_;
+    Thread thread_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    ThreadInitCallback callback_;
+};
